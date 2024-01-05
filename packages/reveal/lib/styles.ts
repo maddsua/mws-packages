cd8cc4f94700bc160a8b3fada383e8ab725a36aa
maddsua <maddsuadev@proton.me>
2024-01-05 22:52:42 +0200

    Create styles.ts

---
+++ packages/reveal/lib/styles.ts
@@ -0,0 +1,16 @@
+
+const styleDirectives = [
+	'html, body {overflow-x: hidden}',
+	'* {box-sizing: border-box}'
+];
+
+export const injectStyles = () => {
+
+	if (typeof document !== 'object') 
+		throw new Error('document global object is inaccessible')
+
+	const style = document.createElement('style');
+		style.id = 'reveal-v3-critical-styles';
+		style.innerHTML = styleDirectives.join('\n');
+	document.head.appendChild(style)
+};
