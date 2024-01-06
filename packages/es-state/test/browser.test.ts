f5ca7ca6f3f2f0d0567903fa64ea82fef639c2ff
Daniel <102395937+maddsua@users.noreply.github.com>
2024-01-06 15:02:49 +0000

    Move package es state (#7)
    * Update LICENSE
    
    * Update readme.md
    
    * Update .gitignore
    
    * Create index.ts
    
    * Create persistentState.ts
    
    * Create state.ts
    
    * Create package.json
    
    * Create README.md
    
    * Create browser.html
    
    * Create browser.test.ts
    
    * Create node.test.ts
    
    * Create tsconfig.json

---
+++ packages/es-state/test/browser.test.ts
@@ -0,0 +1,18 @@
+import { PersistentStateRef } from "../lib/persistentState";
+
+const counter = document.querySelector('[data-hook="counter"]') as HTMLElement;
+
+const ref = new PersistentStateRef<number>(0, 'num_record', 'cookie');
+
+counter.textContent = ref.value.toString();
+console.log(ref.value);
+
+ref.watch((value) => {
+	console.log(value);
+	counter.textContent = value.toString()
+});
+
+document.querySelectorAll<HTMLButtonElement>('[data-action]').forEach(item => item.addEventListener('click', () => {
+	const step = item.getAttribute('data-action')?.includes('+') ? 1 : -1;
+	ref.value += step;
+}));
