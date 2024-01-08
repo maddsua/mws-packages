70d7b13e70507be2d3a1bb1f1b6fec6d3ec01ac7
maddsua <maddsuadev@proton.me>
2024-01-08 15:01:11 +0200

    Create index.ts

---
+++ packages/typed-fetch/lib/index.ts
@@ -0,0 +1,10 @@
+import { TypedRequest } from "./api/request";
+import { TypedResponse } from "./api/response";
+import { type InferRequest, requestToTyped } from "./api/infer";
+
+export {
+	TypedRequest,
+	TypedResponse,
+	requestToTyped,
+	InferRequest
+};
