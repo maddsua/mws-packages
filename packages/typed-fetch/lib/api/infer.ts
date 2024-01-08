ed2d3c196c331f0ffc2f37103a316d3205a593b7
maddsua <maddsuadev@proton.me>
2024-01-08 15:00:34 +0200

    Create infer.ts

---
+++ packages/typed-fetch/lib/api/infer.ts
@@ -0,0 +1,22 @@
+import { TypedRequest } from "./request";
+
+export type InferRequest<T extends {
+	data?: object | null;
+	headers?: Record<string, string>;
+	method?: 'POST' | 'GET';
+}> = TypedRequest<T['data'], T['headers'], T['method']>;
+
+export const requestToTyped = async <T extends TypedRequest<any, any, any>> (request: Request) => {
+
+	interface TypedInit {
+		data: T['data'];
+		headers: T['headers'];
+		method: T['method'];
+	};
+
+	return new TypedRequest('', {
+		data: await request.json(),
+		headers: Object.fromEntries(request.headers.entries()),
+		method: request.method
+	} as TypedInit);
+};
