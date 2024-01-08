e6e8912b7952fb340b1ad0cc410d10ee0fa21a14
maddsua <maddsuadev@proton.me>
2024-01-08 15:04:04 +0200

    Update index.ts

--- packages/typed-fetch/lib/index.ts
+++ packages/typed-fetch/lib/index.ts
@@ -1,10 +1,10 @@
 import { TypedRequest } from "./api/request";
 import { TypedResponse } from "./api/response";
-import { type InferRequest, requestToTyped } from "./api/infer";
+import { type InferRequestType, requestToTyped } from "./api/infer";
 
 export {
 	TypedRequest,
 	TypedResponse,
 	requestToTyped,
-	InferRequest
+	InferRequestType
 };
