846bf88a6c1a0a694848050275f73dd25d8096f0
maddsua <maddsuadev@proton.me>
2024-03-13 03:38:26 +0200

    fix for broken imports at captcha-api

--- packages/captcha-api/lib/index.ts
+++ packages/captcha-api/lib/index.ts
@@ -1,5 +1,5 @@
-import { validateReCaptcha } from "./server/index";
-import { loadReCaptcha, executeReCaptchaV3 } from "./client/index";
+import { validateReCaptcha } from "./server/index.js";
+import { loadReCaptcha, executeReCaptchaV3 } from "./client/index.js";
 
 export {
 	validateReCaptcha,
